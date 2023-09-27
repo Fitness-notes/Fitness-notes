3f76b1d3e59f6f9f8c8fcf4528601673db4d1320
Camilo <camilomafioly@gmail.com>
2023-09-27 11:33:47 -0500

    fitnotes

--- firebase.config.js
+++ firebase.config.js
@@ -7,15 +7,7 @@ import {getFirestore} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCsJbJ6KjGNELFhUCX40lSoV073TODwhYA",
-  authDomain: "gymador-f6ca5.firebaseapp.com",
-  projectId: "gymador-f6ca5",
-  storageBucket: "gymador-f6ca5.appspot.com",
-  messagingSenderId: "234066723435",
-  appId: "1:234066723435:web:9601e2cac9d7917871a72e",
-  measurementId: "G-Z3RZ4NHRCG"
-};
+
 
 
 export const App = initializeApp(firebaseConfig);
